f6df446a9d445651b5fbe5ac0f9ecd2ef822a2f6
Jacquin Moon <jacquindev@outlook.com>
2024-11-15 14:32:12 +0700

    :sparkles: feat: added git alias

--- Profile.ps1
+++ Profile.ps1
@@ -26,6 +26,9 @@ if (Get-Command 'oh-my-posh' -ErrorAction SilentlyContinue) {
     } | Out-Null
 }
 
+# Set alias for `Git` before import `Posh-Git` module
+Set-Alias -Name 'g' -Value 'git'
+
 # Posh Modules
 $PoshModules = @('posh-git', 'powershell-yaml', 'Microsoft.PowerShell.SecretManagement', 'Microsoft.PowerShell.SecretStore', 'Terminal-Icons')
 foreach ($module in $PoshModules) {
