e91db773e9a17f96037ca82e13a364c04204b6ad
Jacquin Moon <jacquindev@outlook.com>
2024-11-21 18:39:37 +0700

    :bug: fix: updated .gitmodules

--- clink/oh-my-posh.lua
+++
@@ -1,7 +0,0 @@
-__ohmyposh_dir = os.getenv('USERPROFILE')..'/AppData/Local/Programs/oh-my-posh'
-
-local ohmyposh_themes = __ohmyposh_dir..'/themes'
-local ohmyposh_prompt = clink.promptfilter(1)
-function ohmyposh_prompt:filter(prompt)
-    return load(io.popen("oh-my-posh init cmd --config "..ohmyposh_themes.."/catppuccin_mocha.omp.json"):read("*a"))()
-end
