d766d36f5ee508029e154859db59e7104bdec301
Jacquin Moon <jacquindev@outlook.com>
2024-11-30 05:19:36 +0700

    :zap: perf: do not run Setup.ps1 script but simplifier the update process for apps

--- dotposh/Config/update-windots.ps1
+++ dotposh/Config/update-windots.ps1
@@ -1,27 +1,87 @@
 # cSpell:disable
 
+# function runsetup {
+#     gsudo "$env:DOTFILES\Setup.ps1"
+# }
+
+# function Update-Windots {
+#     # Check if `gum` command exists, else exit
+#     if (-not (Get-Command gum -ErrorAction SilentlyContinue)) {
+#         Write-Warning "Command not found: gum. Exiting..."
+#         return
+#     }
+
+#     $currentDir = "$(Get-Location)"
+
+#     Set-Location "$env:DOTFILES"
+
+#     # Update local profile with GitHub repository
+#     gum spin --title="Stashing GitHub repo..." --title.foreground="#b4befe" --spinner.foreground="#cba6f7" --show-error -- git stash
+#     gum spin --title="Pulling latest updates..." --title.foreground="#b4befe" --spinner.foreground="#cba6f7" --show-error -- git pull
+#     gum spin --title="Updating local windots..." --title.foreground="#b4befe" --spinner.foreground="#cba6f7" --show-error -- git stash pop
+
+#     # WinGet
+#     # gum spin --title="Updating WinGet packages..." --title.foreground="#b4befe" --spinner.foreground="#cba6f7" --show-error -- winget update --all --accept-package-agreements --accept-source-agreements
+
+#     # scoop
+#     # gum spin --title="Updating scoop packages..." --title.foreground="#b4befe" --spinner.foreground="#cba6f7" --show-error -- scoop update --all
+
+#     # Clearing oh-my-posh cache files
+#     # gum spin --title="Clearing oh-my-posh cache..." --title.foreground="#b4befe" --spinner.foreground="#cba6f7" --show-error -- oh-my-posh cache clear
+
+#     # Updating wsl
+#     # gum spin --title="Updating WSL..." --title.foreground="#b4befe" --spinner.foreground="#cba6f7" --show-error -- wsl --update --pre-release
+
+#     # Write-Verbose "Capturing any new dependencies"
+#     # if (Get-Command gsudo -ErrorAction SilentlyContinue) {
+#     #     gsudo .\Setup.ps1
+#     # }
+#     # else {
+#     #     Start-Process pwsh -ArgumentList ".\Setup.ps1" -Verb RunAs -WindowStyle Hidden -Wait
+#     # }
+
+#     Set-Location $currentDir
+    
+#     Write-Verbose "Reloading PowerShell profile" 
+#     . $PROFILE.CurrentUserAllHosts
+# }
+
+# Set-Alias -Name 'dotupdate' -Value 'Update-Windots'
+
 function Update-Windots {
     $currentDir = "$(Get-Location)"
 
+    . "$env:DOTFILES\Functions.ps1"
+    
+    # Update local profile with GitHub repository
     Set-Location "$env:DOTFILES"
-    
-    Write-Host "Updating GitHub repository of local windots..." -ForegroundColor DarkGray
-    if (Get-Command gum -ErrorAction SilentlyContinue) {
-        gum spin --title="Stashing GitHub repo..." --title.foreground="" -- git stash
-        gum spin --title="Pulling latest updates..." --title.foreground="" -- git pull
-    }
-    else {
-        git stash | Out-Null
-        git pull | Out-Null
+    gum spin --title="Stashing GitHub repo..." --show-error -- git stash
+    gum spin --title="Pulling latest updates..." --show-error -- git pull
+    gum spin --title="Updating local windots..." --show-error -- git stash pop
+
+    # Update winget packages
+    if (Get-Command winget -ErrorAction SilentlyContinue) {
+        gum spin --title="Updating winget packages..." -- winget update --all
     }
 
-    git stash pop --quiet
-    
-    Write-Host "Capturing any new dependencies..." -ForegroundColor DarkGray
-    Start-Process pwsh -ArgumentList ".\Setup.ps1" -Verb RunAs -WindowStyle Hidden -Wait
+    # Update scoop packages
+    if (Get-Command scoop -ErrorAction SilentlyContinue) {
+        gum spin --title="Updating scoop apps..." -- scoop update --all
+    }
 
-    Write-Host "Reloading PowerShell profile..." -ForegroundColor DarkGray
-    Set-Location $currentDir
+    # Update vscode extensions
+    if (Get-Command code -ErrorAction SilentlyContinue) {
+        gum spin --title="Updating vscode extensions..." -- code --update-extensions
+    }
+
+    # Update github cli extensions
+    if (Get-Command gh -ErrorAction SilentlyContinue) {
+        gum spin --title="Updating gh extensions..." -- gh extension upgrade --all
+    }
+
+    Set-Location "$currentDir"
+
+    Write-Verbose "Reloading PowerShell profile" 
     . $PROFILE.CurrentUserAllHosts
 }
 
