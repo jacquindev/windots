5744c53399ec936b6fa5a44c3b8315108f37b455
Jacquin Moon <jacquindev@outlook.com>
2024-12-01 09:13:46 +0700

    :bug: fix: added vault name ad a required parameter

--- dotposh/Modules/Get-OrCreateSecret.ps1
+++ dotposh/Modules/Get-OrCreateSecret.ps1
@@ -1,61 +1,49 @@
-#requires -Module Microsoft.PowerShell.SecretManagement
-#requires -Module Microsoft.PowerShell.SecretStore
-    
 function Get-OrCreateSecret {
-    <#
-    .SYNOPSIS
-        Gets secret from local vault or creates it if it does not exist.
-    .DESCRIPTION
-        This function retrieves a secret from the local vault or creates it if it does not exist.
-    .Parameter SecretName
-        Name of the secret to retrieve or create.
-        Recommended to use the username or public key / client id.
-    .EXAMPLE
-        $password = Get-OrCreateSecret -SecretName $username
-    .EXAMPLE
-        $clientSecret = Get-OrCreateSecret -SecretName $clientId
-    .LINK
-        https://github.com/scottmckendry/Windots/blob/main/Profile.ps1
-    #>
-    
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
-        [Alias('name')]
-        [string]$SecretName
+        [string]$Name,
+        [Parameter(Mandatory = $true)]
+        [string]$Vault
     )
 
-    $secretFolder = "$(Split-Path $env:DOTFILES)\secrets"
-    if (Test-Path "$secretFolder\secrets.xml") {
-        $secretSecurePath = "$secretFolder\secrets.xml"
-    }
-    else {
-        $secretSecurePath = Read-Host "Enter the path you stored decrypted password file"
+    $automationFile = "$(Split-Path $env:DOTFILES)\secrets\automation.xml"
+    if (Test-Path $automationFile) {
+        $password = Import-Clixml -Path $automationFile
+        Unlock-SecretStore -Password $password
     }
 
-    if (Test-Path $secretSecurePath) {
-        $myPassword = Import-Clixml -Path $secretSecurePath
-        Unlock-SecretStore -Password $myPassword
-    }
+    Write-Host "Retrieving secret" -ForegroundColor Blue -NoNewline
+    Write-Host " $Name " -ForegroundColor Magenta -NoNewline
+    Write-Host "from vault" -ForegroundColor Blue -NoNewline
+    Write-Host " $Vault" -ForegroundColor Magenta -NoNewline
+    Write-Host "..." -ForegroundColor Blue
 
-    ''
-    Write-Host "Retrieve secret" -ForegroundColor "Blue" -NoNewline
-    Write-Host " $SecretName " -ForegroundColor "White" -NoNewline
-    Write-Host "from local vault..." -ForegroundColor "Blue"
-    $SecretValue = Get-Secret $SecretName -AsPlainText -ErrorAction SilentlyContinue
-
+    $SecretValue = Get-Secret -Name $Name -Vault $Vault -AsPlainText -ErrorAction SilentlyContinue
+    
+    
     if (!$SecretValue) {
         $createSecret = Read-Host "No secret found matching $SecretName, create one? (Y/n)"
 
-        if ($createSecret.ToUpper() -eq "Y") {
-            $SecretValue = Read-Host -Prompt "Enter secret value for ($SecretName)" -AsSecureString
-            Set-Secret -Name $SecretName -SecureStringSecret $SecretValue
-            $SecretValue = Get-Secret $SecretName -AsPlainText
+        if ($createSecret.ToUpper() -eq 'Y') {
+            $SecretValue = Read-Host -Prompt "Enter secret value for ($Name)" -AsSecureString
+            Set-Secret -Name $Name -Vault $Vault -SecureStringSecret $SecretValue
+            if ($?) {
+                Write-Host "SUCCESS: Secret" -ForegroundColor Green -NoNewline
+                Write-Host " $Name " -ForegroundColor Magenta -NoNewline
+                Write-Host "added to vault" -ForegroundColor Green -NoNewline
+                Write-Host " $Vault" -ForegroundColor Magenta
+            }
+            else {
+                Write-Error "ERROR: $_"
+            }
         }
         else {
-            throw "Secret not found and not created. Exiting."
+            Write-Warning "Secret not found and not created. Exiting..."
+            Return
         }
     }
-    ''
-    return $SecretValue
+    else {
+        return $SecretValue
+    }
 }
