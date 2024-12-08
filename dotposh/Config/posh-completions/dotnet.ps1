eb78d59b57856c853a598379aaeaa1688decf18e
Jacquin Moon <jacquindev@outlook.com>
2024-12-08 09:07:14 +0700

    :hammer: chore: dotnet powershell completion runs asynchronously

--- dotposh/Config/posh-completions/dotnet.ps1
+++ dotposh/Config/posh-completions/dotnet.ps1
@@ -1,9 +1,11 @@
 # dotnet
 if (Get-Command dotnet -ErrorAction SilentlyContinue) {
-    Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-        param($wordToComplete, $commandAst, $cursorPosition)
-        dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    Register-EngineEvent -SourceIdentifier PowerShell.OnIdle -MaxTriggerCount 1 -Action {
+        Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+            param($wordToComplete, $commandAst, $cursorPosition)
+            dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
+                [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+            }
         }
-    }
+    } | Out-Null
 }
