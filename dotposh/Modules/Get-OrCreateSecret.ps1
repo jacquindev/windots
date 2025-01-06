0c561f038b49e9f7caee4b1b65ae440eafe181df
Jacquin Moon <jacquindev@outlook.com>
2025-01-06 08:08:06 +0700

    :sparkles: feat: use Get-OrCreateSecret without having to identify secretname and secretvault in the command line

--- dotposh/Modules/Get-OrCreateSecret.ps1
+++ dotposh/Modules/Get-OrCreateSecret.ps1
@@ -1,49 +1,98 @@
+#requires -Module Microsoft.PowerShell.SecretManagement
+#requires -Module Microsoft.PowerShell.SecretStore
+
 function Get-OrCreateSecret {
+    <#
+    .SYNOPSIS
+        Get secret from local vault or create if it does not exists.
+    .DESCRIPTION
+        Use external modules: SecretManagement & SecretStore to manage secrets on local machine.
+    .PARAMETER SecretName
+        Name of the secret to get or create.
+    .PARAMETER SecretVault
+        Name of the vault where it stores local secrets.
+    .EXAMPLE
+        Get-OrCreateSecret -SecretName mysecret -SecretVault LocalVault
+    .LINK
+        https://github.com/scottmckendry/Windots/blob/main/Profile.ps1#L178
+        https://learn.microsoft.com/en-us/powershell/utility-modules/secretmanagement/overview?view=ps-modules
+    #>
+
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)]
-        [string]$Name,
-        [Parameter(Mandatory = $true)]
-        [string]$Vault
+        [Alias('n', 'name')][string]$SecretName,
+        [Alias('v', 'vault')][string]$SecretVault
     )
 
-    $automationFile = "$(Split-Path $env:DOTFILES)\secrets\automation.xml"
-    if (Test-Path $automationFile) {
-        $password = Import-Clixml -Path $automationFile
-        Unlock-SecretStore -Password $password
+    if (!$SecretName) {
+        $secretNameList = (Get-SecretInfo).Name
+        if ($secretNameList.Count -eq 1) { $SecretName = $secretNameList }
+        elseif ($secretNameList.Count -gt 1) {
+            if (Get-Command gum -ErrorAction SilentlyContinue) {
+                $SecretName = gum choose --header="Choose a Secret Name:" $secretNameList
+            } elseif (Get-Command fzf -ErrorAction SilentlyContinue) {
+                $SecretName = $secretNameList | fzf --prompt="Select Secret Name >  " --height=~80% --layout=reverse --border --exit-0 --cycle --margin="2,40" --padding=1
+            } else {
+                for ($i = 0; $i -lt $secretNameList.Count; $i++) {
+                    Write-Host "[$i] $($secretNameList[$i])"
+                }
+                $index = $(Write-Host "Enter the corresponding number of Secret Name: " -ForegroundColor Magenta -NoNewline; Read-Host)
+                if ($null -ne $index) {
+                    if ($index -match '^\d+$' -and [int]$index -lt $secretNameList.Count) {
+                        $SecretName = $secretNameList[$index]
+                    } else { return }
+                }
+            }
+        } else {
+            if (Get-Command gum -ErrorAction SilentlyContinue) {
+                $SecretName = gum input --prompt="Input Secret Name: "
+            } else {
+                $SecretName = $(Write-Host "Input Secret Name: " -ForegroundColor Magenta -NoNewline; Read-Host)
+            }
+        }
     }
 
-    Write-Host "Retrieving secret" -ForegroundColor Blue -NoNewline
-    Write-Host " $Name " -ForegroundColor Magenta -NoNewline
-    Write-Host "from vault" -ForegroundColor Blue -NoNewline
-    Write-Host " $Vault" -ForegroundColor Magenta -NoNewline
-    Write-Host "..." -ForegroundColor Blue
+    if (!$SecretVault) {
+        $secretVaultList = (Get-SecretVault).Name
+        if ($secretVaultList.Count -eq 1) { $SecretVault = $secretVaultList }
+        elseif ($secretVaultList.Count -gt 1) {
+            if (Get-Command gum -ErrorAction SilentlyContinue) {
+                $SecretVault = gum choose --header="Choose a Secret Vault:" $secretVaultList
+            } elseif (Get-Command fzf -ErrorAction SilentlyContinue) {
+                $SecretVault = $secretVaultList | fzf --prompt="Select Secret Vault >  " --height=~80% --layout=reverse --border --exit-0 --cycle --margin="2,40" --padding=1
+            } else {
+                for ($j = 0; $j -lt $secretVaultList.Count; $j++) {
+                    Write-Host "[$j] $($secretVaultList[$j])"
+                }
+                $index = $(Write-Host "Enter the corresponding number of Secret Vault: " -ForegroundColor Magenta -NoNewline; Read-Host)
+                if ($null -ne $index) {
+                    if ($index -match '^\d+$' -and [int]$index -lt $secretVaultList.Count) {
+                        $SecretVault = $secretVaultList[$index]
+                    } else { return }
+                }
+            }
+        } else {
+            if (Get-Command gum -ErrorAction SilentlyContinue) {
+                $SecretVault = gum input --prompt="Input Secret Vault name: "
+            } else {
+                $SecretVault = $(Write-Host "Input Secret Vault name: " -ForegroundColor Magenta -NoNewline; Read-Host)
+            }
+            Set-SecretStoreConfiguration -Scope CurrentUser -Authentication None -Confirm:$False
+            Register-SecretVault -Name $SecretVault -ModuleName Microsoft.PowerShell.SecretStore -DefaultVault
+        }
+    }
 
-    $SecretValue = Get-Secret -Name $Name -Vault $Vault -AsPlainText -ErrorAction SilentlyContinue
-    
-    
+    $SecretValue = Get-Secret -Name $SecretName -Vault $SecretVault -AsPlainText -ErrorAction SilentlyContinue
+
     if (!$SecretValue) {
-        $createSecret = Read-Host "No secret found matching $SecretName, create one? (Y/n)"
+        $createSecret = $(Write-Host "No secret found matching $SecretName. Create one? (Y/n) " -ForegroundColor Magenta -NoNewline; Read-Host)
 
         if ($createSecret.ToUpper() -eq 'Y') {
-            $SecretValue = Read-Host -Prompt "Enter secret value for ($Name)" -AsSecureString
-            Set-Secret -Name $Name -Vault $Vault -SecureStringSecret $SecretValue
-            if ($?) {
-                Write-Host "SUCCESS: Secret" -ForegroundColor Green -NoNewline
-                Write-Host " $Name " -ForegroundColor Magenta -NoNewline
-                Write-Host "added to vault" -ForegroundColor Green -NoNewline
-                Write-Host " $Vault" -ForegroundColor Magenta
-            }
-            else {
-                Write-Error "ERROR: $_"
-            }
-        }
-        else {
-            Write-Warning "Secret not found and not created. Exiting..."
-            Return
-        }
+            $SecretValue = Read-Host -Prompt "Enter secret value for ($SecretName)" -AsSecureString
+            Set-Secret -Name $SecretName -Vault $SecretVault -SecureStringSecret $SecretValue
+            $SecretValue = Get-Secret -Name $SecretName -AsPlainText -Vault $SecretVault
+        } else { throw "Secret not found and not created, exiting" }
     }
-    else {
-        return $SecretValue
-    }
+
+    return $SecretValue
 }
