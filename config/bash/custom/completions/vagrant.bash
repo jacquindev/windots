8fb931cef3896535de2465699f2fe1f5aedf2f62
Jacquin Moon <jacquindev@outlook.com>
2025-02-15 17:20:59 +0700

    :sparkles: feat: move git-bash completions folder into a submodule

--- config/bash/custom/completions/vagrant.bash
+++
@@ -1,4 +0,0 @@
-vagrant_completion_file="$(find "/c/Program Files/Vagrant" -type f -name 'completion.sh' | head -n 1)"
-if [[ -f "$vagrant_completion_file" ]]; then
-	source "$vagrant_completion_file"
-fi
